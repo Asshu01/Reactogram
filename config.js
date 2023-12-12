3b025cf90d2c66116a998d80b4ef3c7fb6b2069b
Ashmita Shrivas <109226173+Asshu01@users.noreply.github.com>
2023-12-12 13:51:20 +0530

    Mongo db url changed

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    MONGODB_URL : "mongodb://localhost:27017",
+    MONGODB_URL : "mongodb+srv://shrivasashmita:Ashmita2002@ashmita.ff85pnw.mongodb.net/Reactogram?retryWrites=true&w=majority",
     JWT_SECRET : "xlkjsdfnjjjjnnnnnnknvlsdkj"
 }
